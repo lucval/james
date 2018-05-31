c41d429cde3d5e1d8882663ba83a9564c77685c1
Luca Valtulina <l.valtulina@speakup.nl>
2018-05-31 17:04:54 +0200

    Add User resource to models.
    This collection will be used to store users credentials (password
    are stored hashed together with the used salt) in order to perform
    login and authentication on the API.
    
    After logging in, a user receives a JWT to be used in further actions
    against the API. When the JWT token is expired, a new login is required.

--- loan/models.py
+++ loan/models.py
@@ -14,20 +14,32 @@ Version: 0.1.0
 Python Version 2.7
 """
 
-import math, uuid
+import bcrypt, jwt, math, uuid
+from datetime import datetime, timedelta
 from dateutil import parser
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import validates
+from sqlalchemy.orm.exc import NoResultFound
 
 from loan.app import app
 
 db = SQLAlchemy(app)
 
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS256'
+JWT_EXP_DELTA_SECONDS = 600
+
 ### Custom exceptions ###
 
 class BadRequest(Exception):
     status_code = 400
+
+class Unauthorized(Exception):
+    status_code = 401
+
+class Forbidden(Exception):
+    status_code = 401
 
 class NotFound(Exception):
     status_code = 404
@@ -36,6 +48,78 @@ class Conflict(Exception):
     status_code = 409
 
 ### DB models ###
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.String(36), primary_key=True, autoincrement=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    h_password = db.Column(db.String(128), nullable=False)
+    salt = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return "<User {}: {}>".format(self.id, self.email)
+
+    @validates('email')
+    def _validate_input(self, key, email):
+        if not email:
+            raise BadRequest("'{}' field required".format(key))
+
+        return email
+
+    def login(self, email, password):
+        """Login a user.
+
+        :return: a signed JWT if user has granted access.
+        :raise Forbidden: when user has no granted access.
+        """
+        try:
+            # Lookup user
+            user = db.session.query(User).filter(User.email == email).one()
+
+            # Generate hashed password using stored salt
+            h_password = bcrypt.hashpw(password.encode('utf-8'), user.salt.encode('utf-8')).decode()
+            if user.h_password != h_password:
+                raise NoResultFound
+
+        except NoResultFound:
+            # A NotFound will be a security offence here as we are giving away which users exist in our DB.
+            raise BadRequest("User '{}' login failed".format(self.email))
+
+        payload = {
+            'user_id': user.id,
+            'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS)
+        }
+        jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
+
+        return jwt_token.decode('utf-8')
+
+    def authenticate(self, jwt_token):
+        if not jwt_token:
+            raise Forbidden("Access denied")
+
+        try:
+            payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        except (jwt.DecodeError, jwt.ExpiredSignatureError):
+            raise BadRequest("Token is invalid")
+
+        user = User.query.get(payload['user_id'])
+        if not user:
+           raise Unauthorized("User is not authorized")
+
+    def add(self, password):
+        """Add a user record (if not yet existing)."""
+
+        # Generate user ID
+        self.id = str(uuid.uuid4())
+        self.salt = bcrypt.gensalt()
+        self.h_password = bcrypt.hashpw(password.encode('utf-8'), self.salt.encode('utf-8')).decode()
+
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except IntegrityError as e:
+            db.session.rollback()
 
 class Loan(db.Model):
     __tablename__ = 'loans'
@@ -88,7 +172,6 @@ class Loan(db.Model):
         :raise NotFound: when a loan record is not found.
         """
         loan = self.query.get(loan_id)
-
         if not loan:
             raise NotFound("Loan {} not found".format(loan_id))
 
@@ -97,17 +180,17 @@ class Loan(db.Model):
     def create(self):
         """Add a loan record.
 
-        :raise Conflict: when a loan record could not be created due to a database integrity error.
+        :raise BadRequest: when a loan record could not be created due to a database integrity error.
         """
         # Generate loan ID
-        self.loan_id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
 
         try:
             db.session.add(self)
             db.session.commit()
         except IntegrityError as e:
             db.session.rollback()
-            raise Conflict(e.message)
+            raise BadRequest("Invalid loan record provided")
 
     def calculate_installment(self):
         """Calculate the monthly loan payment.
@@ -162,8 +245,8 @@ class Payment(db.Model):
     def create(self):
         """Add a payment record.
 
-        :raise BadRequest: when payment's date is prior to loan's initial date.
-        :raise Conflict: when a payment record could not be created due to a database integrity error.
+        :raise BadRequest: when payment's date is prior to loan's initial date or when a payment record could not be
+        created due to a database integrity error.
         """
         loan = Loan().get(self.loan_id)
 
@@ -175,7 +258,7 @@ class Payment(db.Model):
             db.session.commit()
         except IntegrityError as e:
             db.session.rollback()
-            raise Conflict(e.message)
+            raise BadRequest("Invalid payment record provided")
 
     def list(self, loan_id, until_date=None, only_made=True):
         """List payments record.
